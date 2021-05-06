f6dac9a1251a4d027f67523e4ed17810b9b4531d
Ginger Alford <gingeralford@gmail.com>
2021-05-06 19:03:47 -0400

    intital heroku push

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -1,15 +1,15 @@
-let APIURL: string = 'http://127.0.0.1:3000';
+let APIURL: string = '';
 
-// switch (window.location.hostname) {
-//     // this is the local host name of your react app
-//     case 'localhost' || '127.0.0.1':
-//         // this is the local host name of your API
-//         APIURL = 'http://localhost:3000';
-//         break;
-//     // this is the deployed react application
-//     case 'ga-beast-mode.herokuapp.com':
-//         // this is the full url of your deployed API
-//         APIURL = 'https://ga-beast-mode-server.herokuapp.com'
-// }
+switch (window.location.hostname) {
+    // this is the local host name of your react app
+    case 'localhost' || '127.0.0.1':
+        // this is the local host name of your API
+        APIURL = 'http://localhost:3001';
+        break;
+    // this is the deployed react application
+    case 'table-tempo.herokuapp.com':
+        // this is the full url of your deployed API
+        APIURL = 'https://table-tempo-server.herokuapp.com'
+}
 
 export default APIURL;
